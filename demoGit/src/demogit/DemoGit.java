019af9706b3d9820164f49f09dd7b7bd374050fa
ElDuderino420 <david@pfg.dk>
2015-09-22 10:20:58 +0200

    added "..."

---
+++ demoGit/src/demogit/DemoGit.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package demogit;
+
+/**
+ *
+ * @author David
+ */
+public class DemoGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+    
+        System.out.println("Test 9951");
+        System.out.println("we are almost done testing...");
+    }
+    
+}
